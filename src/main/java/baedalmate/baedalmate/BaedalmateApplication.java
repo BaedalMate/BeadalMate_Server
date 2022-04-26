59bf5324fe408c11b5847368d3ea948ffb27d738
EomYoosang <dbtkd1801@gmail.com>
2022-04-27 02:59:34 +0900

    initial commit

---
+++ src/main/java/baedalmate/baedalmate/BaedalmateApplication.java
@@ -0,0 +1,13 @@
+package baedalmate.baedalmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaedalmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaedalmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/baedalmate/baedalmate/BaedalmateApplicationTests.java
@@ -0,0 +1,13 @@
+package baedalmate.baedalmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaedalmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
