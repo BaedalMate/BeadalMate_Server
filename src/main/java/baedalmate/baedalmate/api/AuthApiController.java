ab2ab4ecb5dc4481940b59a7941f0f58570195db
EomYoosang <dbtkd1801@gmail.com>
2022-06-20 19:29:34 +0900

    feat(login): Change login request
    Login 요청 방식을 GET에서 POST로 변경
    기존 Authorization 헤더에 담던 소셜 액세스 토큰을 requestBody에 담음
    Response에 refreshToken 포함
    로그인 요청 Swagger 문서화

---
+++ src/main/java/baedalmate/baedalmate/api/AuthApiController.java
@@ -0,0 +1,36 @@
+package baedalmate.baedalmate.api;
+
+import baedalmate.baedalmate.oauth.provider.TokenProvider;
+import baedalmate.baedalmate.repository.UserRepository;
+import io.swagger.annotations.ApiOperation;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthApiController {
+
+    private final UserRepository userRepository;
+    private final TokenProvider tokenProvider;
+
+    @ApiOperation("로그인")
+    @PostMapping("/login/oauth2/kakao")
+    public tokenDto fakeLogin(@RequestBody LoginDto loginDto) {
+        throw new IllegalStateException("This method shouldn't be called. It's implemented by Spring Security filters.");
+    }
+
+    @Data
+    static class LoginDto {
+        private String kakaoAccessToken;
+    }
+
+    @Data
+    static class tokenDto {
+        private String accessToken;
+        private String refreshToken;
+    }
+}
