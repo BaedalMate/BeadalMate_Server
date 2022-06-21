b84ef0f1c20bb887e8dca2ca0a525917ee351e94
EomYoosang <dbtkd1801@gmail.com>
2022-06-21 18:36:11 +0900

    fix(swagger): Update RecruitApiController
    예상 json 결과가 출력되지 않음. Result 객체를 클래스 내부 static 클래스로 변경하고 리스트 내용인 RecruitDto 클래스에 @Data annotation 추가 및 예시 값 추가.

--- src/main/java/baedalmate/baedalmate/api/RecruitApiController.java
+++ src/main/java/baedalmate/baedalmate/api/RecruitApiController.java
@@ -1,9 +1,11 @@
 package baedalmate.baedalmate.api;
 
-import baedalmate.baedalmate.dto.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,14 +33,39 @@ public class RecruitApiController {
         return new Result(collect);
     }
 
+    @Data
+    @AllArgsConstructor
+    @Schema
+    static class Result {
+        @Schema(name = "결과 리스트")
+        private List<RecruitDto> data;
+    }
+
+    @Data
+    @Schema
     static class RecruitDto {
+        @Schema(name = "해당 모집글 id", example = "1")
         private Long id;
+
+        @Schema(name = "식당 이름", example = "도미노피자")
         private String restaurantName;
+
+        @Schema(name = "최소 주문 금액", example = "15000")
         private int minPrice;
+
+        @Schema(name = "배달비", example = "3000")
         private int deliveryFee;
+
+        @Schema(name = "글 작성 시간")
         private Date createDate;
+
+        @Schema(name = "마감 시간")
         private Date deadlineDate;
+
+        @Schema(name = "예상 배달 시간", example = "20~30분")
         private String estimateDeliveryTime;
+
+        @Schema(name = "유저 평점", example = "4.1")
         private float userScore;
     }
 }
