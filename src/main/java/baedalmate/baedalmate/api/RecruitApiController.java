02d7d731f8b04efd664582ca688c478bc343e9b6
EomYoosang <dbtkd1801@gmail.com>
2022-06-21 18:19:07 +0900

    fix(swagger): Update Controllers
    요청 메서드에 Mapping annotation을 추가하지 않아 명세가 나타나지 않음. 각 요청에 Mapping 추가.
    Pageable 클래스를 data.domain이 아닌 awt.print의 Pageble을 임포트하여 출력되지 않음. data.domain으로 변경.

--- src/main/java/baedalmate/baedalmate/api/RecruitApiController.java
+++ src/main/java/baedalmate/baedalmate/api/RecruitApiController.java
@@ -4,23 +4,28 @@ import baedalmate.baedalmate.dto.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.print.Pageable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Api(tags = {"모집글 api"})
 @RestController
+@RequestMapping("/api/v1")
 public class RecruitApiController {
 
     @ApiOperation(value = "모집글 리스트 조회")
+    @GetMapping(value = "/recruit/list")
     public Result getRecruitList(
             @ApiParam(value = "카테고리별 조회(일단 사용x)")
             @RequestParam(required = false) Long categoryId,
-            Pageable pageable
+            @ApiParam(value = "예시: {ip}:8080/production/list?page=0&size=5&sort=view,DESC")
+                    Pageable pageable
     ) {
         List<RecruitDto> collect = new ArrayList<>();
         return new Result(collect);
