341263749e417ff09501ede7b66d0980a5970f4a
EomYoosang <dbtkd1801@gmail.com>
2022-07-01 02:08:57 +0900

    fix(user): Change table name of user

--- src/main/java/baedalmate/baedalmate/oauth/service/LoadUserService.java
+++ src/main/java/baedalmate/baedalmate/oauth/service/LoadUserService.java
@@ -8,6 +8,7 @@ import baedalmate.baedalmate.oauth.service.strategy.KakaoLoadStrategy;
 import baedalmate.baedalmate.oauth.service.strategy.SocialLoadStrategy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 @Service
