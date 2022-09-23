9e3fb14aba58a627b9e546b0fda0c3b2f67355a6
EomYoosang <dbtkd1801@gmail.com>
2022-09-24 02:32:23 +0900

    feat(recruit): Update recruit list api

--- src/main/java/baedalmate/baedalmate/api/RecruitApiController.java
+++ src/main/java/baedalmate/baedalmate/api/RecruitApiController.java
@@ -10,11 +10,17 @@ import baedalmate.baedalmate.service.UserService;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -81,37 +87,35 @@ public class RecruitApiController {
         return new CreateRecruitResponse(id);
     }
 
-//    @ApiOperation(value = "모집글 리스트 조회")
-//    @GetMapping(value = "/recruit/list")
-//    public RecruitList getRecruitList(
-//            @ApiParam(value = "카테고리별 조회(구현x)")
-//            @RequestParam(required = false) Long categoryId,
-//            @PageableDefault(size = 10)
-//            @ApiParam(value = "예시: {ip}:8080/recruit/list?page=0&size=5&sort=createDate,DESC")
-//            @SortDefault.SortDefaults({
-//                    @SortDefault(sort = "createDate", direction = Sort.Direction.DESC)
-//            })
-//                    Pageable pageable) {
-//        Page<Recruit> recruits = recruitService.findAll(pageable);
-//
-//        List<RecruitDto> collect = recruits.getContent().stream()
-//                .map(r -> new RecruitDto(
-//                        r.getId(),
-//                        r.getRestaurant(),
-//                        r.getMinPrice(),
-//                        r.getMinPeople(),
-//                        r.getCurrentPeople(),
-//                        r.getDeliveryFee(),
-//                        r.getCreateDate(),
-//                        r.getDeadlineDate(),
-//                        r.getUser().getNickname(),
-//                        r.getUser().getScore(),
-//                        r.getDormitory().getName(),
-//                        r.getTitle()
-//                ))
-//                .collect(Collectors.toList());
-//        return new RecruitList(collect);
-//    }
+    @ApiOperation(value = "모집글 리스트 조회")
+    @GetMapping(value = "/recruit/list")
+    public RecruitList getRecruitList(
+            @ApiParam(value = "카테고리별 조회(구현x)")
+            @RequestParam(required = false) Long categoryId,
+            @PageableDefault(size = 10)
+            @ApiParam(value = "예시: {ip}:8080/recruit/list?page=0&size=5&sort=createDate,DESC")
+            @SortDefault.SortDefaults({
+                    @SortDefault(sort = "createDate", direction = Sort.Direction.DESC)
+            })
+                    Pageable pageable) {
+        Page<Recruit> recruits = recruitService.findAll(pageable);
+
+        List<RecruitDto> collect = recruits.getContent().stream()
+                .map(r -> new RecruitDto(
+                        r.getId(),
+                        r.getPlace().getName(),
+                        r.getMinPeople(),
+                        r.getCurrentPeople(),
+                        r.getCriteria(),
+                        r.getCreateDate(),
+                        r.getDeadlineDate(),
+                        r.getUser().getScore(),
+                        r.getDormitory().getName(),
+                        r.getTitle()
+                ))
+                .collect(Collectors.toList());
+        return new RecruitList(collect);
+    }
 
     @Data
     @Schema
@@ -224,10 +228,7 @@ public class RecruitApiController {
         private Long id;
 
         @Schema(description = "식당 이름", example = "도미노피자")
-        private String restaurantName;
-
-        @Schema(description = "최소 주문 금액", example = "15000")
-        private int minPrice;
+        private String place;
 
         @Schema(description = "최소 인원", example = "4")
         private int minPeople;
@@ -235,8 +236,8 @@ public class RecruitApiController {
         @Schema(description = "현재 인언", example = "1")
         private int currentPeople;
 
-        @Schema(description = "배달비", example = "3000")
-        private int deliveryFee;
+        @Schema(description = "마감 기준")
+        private Criteria criteria;
 
         @Schema(description = "글 작성 시간")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
@@ -245,9 +246,6 @@ public class RecruitApiController {
         @Schema(description = "마감 시간")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
         private LocalDateTime deadlineDate;
-
-        @Schema(description = "유저 이름", example = "유상")
-        private String username;
 
         @Schema(description = "유저 평점", example = "4.1")
         private float userScore;
