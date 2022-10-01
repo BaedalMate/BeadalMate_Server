8cf8a64a0053c5d7cf38117e4f964c0656af22ff
EomYoosang <dbtkd1801@gmail.com>
2022-10-01 22:09:57 +0900

    feat(recruit): Update RecruitRepository
    Update find methods in recruitRepository to use @Query annotation and jpql.
    Find Recruit list by category.

--- src/main/java/baedalmate/baedalmate/service/RecruitService.java
+++ src/main/java/baedalmate/baedalmate/service/RecruitService.java
@@ -1,13 +1,16 @@
 package baedalmate.baedalmate.service;
 
+import baedalmate.baedalmate.domain.Dormitory;
 import baedalmate.baedalmate.domain.Recruit;
 import baedalmate.baedalmate.repository.RecruitJpaRepository;
 import baedalmate.baedalmate.repository.RecruitRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -27,7 +30,35 @@ public class RecruitService {
         return recruitRepository.findOne(recruitId);
     }
 
-    public Page<Recruit> findAll(Pageable pageable) {
-        return recruitJpaRepository.findAll(pageable);
+    public List<Recruit> findAll(Pageable pageable) {
+        String sort = pageable.getSort().toString();
+        if(sort.contains("score")) {
+            return recruitJpaRepository.findAllUsingJoinOrderByScore(pageable);
+        }
+        if(sort.contains("deadlineDate")) {
+            return recruitJpaRepository.findAllUsingJoinOrderByDeadlineDate(pageable);
+        }
+        if(sort.contains("view")) {
+            return recruitJpaRepository.findAllUsingJoinOrderByView(pageable);
+        }
+        return new ArrayList<Recruit>();
+    }
+
+    public List<Recruit> findAllWithTag(Dormitory dormitory, Pageable pageable) {
+        return recruitJpaRepository.findAllWithTagsUsingJoinOrderByDeadlineDate(dormitory, pageable);
+    }
+
+    public List<Recruit> findAllByCategory(Long categoryId, Pageable pageable) {
+        String sort = pageable.getSort().toString();
+        if(sort.contains("score")) {
+            return recruitJpaRepository.findAllByCategoryUsingJoinOrderByScore(categoryId, pageable);
+        }
+        if(sort.contains("deadlineDate")) {
+            return recruitJpaRepository.findAllByCategoryUsingJoinOrderByDeadlineDate(categoryId, pageable);
+        }
+        if(sort.contains("view")) {
+            return recruitJpaRepository.findAllByCategoryUsingJoinOrderByView(categoryId, pageable);
+        }
+        return new ArrayList<Recruit>();
     }
 }
