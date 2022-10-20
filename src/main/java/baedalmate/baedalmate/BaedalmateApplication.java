dc93882e865f521514faa7fba98f82d40d07320e
EomYoosang <dbtkd1801@gmail.com>
2022-10-20 17:40:16 +0900

    fix(timezone): Update timezone

--- src/main/java/baedalmate/baedalmate/BaedalmateApplication.java
+++ src/main/java/baedalmate/baedalmate/BaedalmateApplication.java
@@ -5,9 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableConfigurationProperties(AppProperties.class)
 @SpringBootApplication
 public class BaedalmateApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaedalmateApplication.class, args);
