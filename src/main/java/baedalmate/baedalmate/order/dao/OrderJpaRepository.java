f9029e37460feac98204d70a8cdc770807fe0d6b
EomYoosang <dbtkd1801@gmail.com>
2022-11-14 19:14:06 +0900

    refactor: Update method name.
    Update name of method "findAllByRecruitId" in OrderJpaRepository to "findAllByRecruitIdUsingJoin"

--- src/main/java/baedalmate/baedalmate/order/dao/OrderJpaRepository.java
+++ src/main/java/baedalmate/baedalmate/order/dao/OrderJpaRepository.java
@@ -10,5 +10,9 @@ import java.util.List;
 public interface OrderJpaRepository extends JpaRepository<Order, Long> {
 
     @Query("select o from Order o join fetch o.recruit join fetch o.menus join fetch o.user where o.recruit.id = :id")
-    List<Order> findAllByRecruitId(@Param("id") Long recruitId);
+    List<Order> findAllByRecruitIdUsingJoin(@Param("id") Long recruitId);
+
+    @Query("select o from Order o join fetch o.recruit join fetch o.menus join fetch o.user where o.recruit.id = :recruitId and o.user.id = :userId")
+    Order findByUserIdAndRecruitIdUsingJoin(@Param("userId") Long userId, @Param("recruitId") Long recruitId);
+
 }
