7d52477f33e4fee8e5d5901943c0f5526df38c5c
EomYoosang <dbtkd1801@gmail.com>
2022-11-21 04:37:14 +0900

    feat(schedule): Log date

--- src/main/java/baedalmate/baedalmate/recruit/schedule/DynamicScheduler.java
+++ src/main/java/baedalmate/baedalmate/recruit/schedule/DynamicScheduler.java
@@ -6,6 +6,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 
 @Component
 @EnableScheduling
@@ -14,8 +16,9 @@ public class DynamicScheduler {
 
     private final RecruitService recruitService;
 
-    @Scheduled(cron = "0 * * * * *")
+    @Scheduled(cron = "0/30 * * * * *")
     public void run() {
+        System.out.println(LocalDateTime.now());
         recruitService.closeBySchedule();
     }
 }
