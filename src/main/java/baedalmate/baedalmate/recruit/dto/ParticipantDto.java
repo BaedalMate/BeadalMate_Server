c6fdaf2808cc03094d6186d4a3f87e655ccfdc62
EomYoosang <dbtkd1801@gmail.com>
2022-11-27 20:30:40 +0900

    feat(participant): Get participants api

---
+++ src/main/java/baedalmate/baedalmate/recruit/dto/ParticipantDto.java
@@ -0,0 +1,19 @@
+package baedalmate.baedalmate.recruit.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Schema
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParticipantDto {
+    @Schema(description = "유저 id")
+    private Long userId;
+    @Schema(description = "유저 닉네임")
+    private String nickname;
+    @Schema(description = "유저 프로필 이미지")
+    private String profileImage;
+}
---
+++ src/main/java/baedalmate/baedalmate/recruit/dto/ParticipantsDto.java
@@ -0,0 +1,18 @@
+package baedalmate.baedalmate.recruit.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Schema
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParticipantsDto {
+    @Schema(description = "모집글 id")
+    private Long recruitId;
+    private List<ParticipantDto> participants;
+}
