74a4bcdab3d2a03eeff0f990b55d2a082af75f9a
EomYoosang <dbtkd1801@gmail.com>
2022-12-08 17:37:47 +0900

    feat(review): Review target api
    Add api get review target.

--- src/main/java/baedalmate/baedalmate/review/service/ReviewService.java
+++ src/main/java/baedalmate/baedalmate/review/service/ReviewService.java
@@ -1,8 +1,13 @@
 package baedalmate.baedalmate.review.service;
 
+import baedalmate.baedalmate.errors.exceptions.AccessDeniedException;
 import baedalmate.baedalmate.errors.exceptions.InvalidApiRequestException;
+import baedalmate.baedalmate.order.dao.OrderJpaRepository;
+import baedalmate.baedalmate.order.domain.Order;
 import baedalmate.baedalmate.recruit.dao.RecruitRepository;
 import baedalmate.baedalmate.recruit.domain.Recruit;
+import baedalmate.baedalmate.recruit.dto.ParticipantDto;
+import baedalmate.baedalmate.recruit.dto.ParticipantsDto;
 import baedalmate.baedalmate.review.dao.ReviewJpaRepository;
 import baedalmate.baedalmate.review.domain.Review;
 import baedalmate.baedalmate.review.dto.CreateReviewDto;
@@ -13,6 +18,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -20,7 +30,28 @@ public class ReviewService {
     private final ReviewJpaRepository reviewJpaRepository;
     private final UserJpaRepository userJpaRepository;
     private final RecruitRepository recruitRepository;
+    private final OrderJpaRepository orderJpaRepository;
 
+    public ParticipantsDto getTarget(Long userId, Long recruitId) {
+        AtomicBoolean participate = new AtomicBoolean(false);
+        List<Order> orders = orderJpaRepository.findAllByRecruitIdUsingJoin(recruitId);
+        List<ParticipantDto> participants = new ArrayList<>();
+        for (Order o : orders) {
+            if (o.getUser().getId() == userId) {
+                participate.set(true);
+                continue;
+            }
+            participants.add(
+                    new ParticipantDto(o.getUser().getId(), o.getUser().getNickname(), o.getUser().getProfileImage())
+            );
+        }
+        if (participate.get() == false) {
+            throw new AccessDeniedException("User is not participant");
+        }
+        return new ParticipantsDto(recruitId, participants);
+    }
+
+    @Transactional
     public void create(Long userId, CreateReviewDto createReviewDto) {
         // 유저조회
         User user = userJpaRepository.findById(userId).get();
