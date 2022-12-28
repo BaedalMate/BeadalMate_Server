747fff4b68ed9597731ad9d349d50c8e3b141cdb
EomYoosang <dbtkd1801@gmail.com>
2022-12-29 00:32:11 +0900

    feat: Update error case

--- src/main/java/baedalmate/baedalmate/errors/handler/GlobalExceptionHandler.java
+++ src/main/java/baedalmate/baedalmate/errors/handler/GlobalExceptionHandler.java
@@ -42,9 +42,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException e) {
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException e) {
         ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
-        return handleExceptionInternal(errorCode, e.getMessage());
+        return handleExceptionInternal(errorCode);
     }
 
     @Override
@@ -53,15 +53,15 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             HttpHeaders headers,
             HttpStatus status,
             WebRequest request) {
-        ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
+        ErrorCode errorCode = CommonErrorCode.API_ARGUMENT_NOT_VALID;
         return handleExceptionInternal(e, errorCode);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleAllException(Exception ex) {
-        ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
-        return handleExceptionInternal(errorCode);
-    }
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<Object> handleAllException(Exception ex) {
+//        ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
+//        return handleExceptionInternal(errorCode);
+//    }
 
     private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
         return ResponseEntity.status(errorCode.getHttpStatus().value())
@@ -89,20 +89,20 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     private ResponseEntity<Object> handleExceptionInternal(BindException e, ErrorCode errorCode) {
         return ResponseEntity.status(errorCode.getHttpStatus().value())
-                .body(makeErrorResponse(e, errorCode));
+                .body(makeErrorResponse(errorCode));
     }
 
-    private ErrorResponse makeErrorResponse(BindException e, ErrorCode errorCode) {
-        List<ErrorResponse.ValidationError> validationErrorList = e.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(ErrorResponse.ValidationError::of)
-                .collect(Collectors.toList());
-
-        return ErrorResponse.builder()
-                .code(errorCode.getHttpStatus().value())
-                .message(errorCode.getMessage())
-                .errors(validationErrorList)
-                .build();
-    }
+//    private ErrorResponse makeErrorResponse(BindException e, ErrorCode errorCode) {
+//        List<ErrorResponse.ValidationError> validationErrorList = e.getBindingResult()
+//                .getFieldErrors()
+//                .stream()
+//                .map(ErrorResponse.ValidationError::of)
+//                .collect(Collectors.toList());
+//
+//        return ErrorResponse.builder()
+//                .code(errorCode.getHttpStatus().value())
+//                .message(errorCode.getMessage())
+//                .errors(validationErrorList)
+//                .build();
+//    }
 }
