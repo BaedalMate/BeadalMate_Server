cc55c10a0a2e464713bff2be425d3598baff2396
EomYoosang <dbtkd1801@gmail.com>
2023-01-20 21:38:38 +0900

    feat: Total count
    Add total count when search by tag.

---
+++ src/main/java/baedalmate/baedalmate/recruit/dto/RecruitListWithLastAndCount.java
@@ -0,0 +1,18 @@
+package baedalmate.baedalmate.recruit.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Schema
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecruitListWithLastAndCount {
+    private List<?> recruitList;
+    private boolean isLast;
+    private long total;
+}
