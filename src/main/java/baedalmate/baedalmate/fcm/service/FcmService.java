3238936f965be94bdf062a556603a67e3432cacd
EomYoosang <dbtkd1801@gmail.com>
2023-02-07 07:10:08 +0900

    feat: Update fcm

--- src/main/java/baedalmate/baedalmate/fcm/service/FcmService.java
+++ src/main/java/baedalmate/baedalmate/fcm/service/FcmService.java
@@ -60,26 +60,37 @@ public class FcmService {
     public void sendByTokenList(List<String> tokenList, String title, String description, String image, String type, Long chatRoomId) {
 
         // 메시지 만들기
+        // fcm multiMessage 객체 생성 하여 보냄
+        MulticastMessage message = MulticastMessage.builder()
+                .putData("fcm_type", "NOTIFICATION") // 알람 종류 타입
+                .putData("title", title)
+                .putData("body", description)
+                .putData("image", image)
+                .putData("chatRoomId", chatRoomId.toString())
+                .setAndroidConfig(AndroidConfig.builder().setPriority(AndroidConfig.Priority.HIGH).build())
+                .addAllTokens(tokenList)
+//                .addToken(token)  // 단일 토큰일 경우
+                .build();
 
-        Map<String, String> data = new HashMap<>();
-        data.put("time", LocalDateTime.now().toString());
-        data.put("image", image);
-        data.put("type", type);
-        if(chatRoomId != null) {
-            data.put("chatRoomId", chatRoomId.toString());
-        }
-        List<Message> messages = tokenList.stream().map(token -> Message.builder()
-                .putAllData(data)
-                .setNotification(new Notification(title, description))
-                .setToken(token)
-                .build()).collect(Collectors.toList());
+//        Map<String, String> data = new HashMap<>();
+//        data.put("time", LocalDateTime.now().toString());
+//        data.put("image", image);
+//        data.put("type", type);
+//        if(chatRoomId != null) {
+//            data.put("chatRoomId", chatRoomId.toString());
+//        }
+//        List<Message> messages = tokenList.stream().map(token -> Message.builder()
+//                .putAllData(data)
+//                .setNotification(Notification.builder().setTitle(title).setBody(description).setImage(image).build())
+//                .setToken(token)
+//                .build()).collect(Collectors.toList());
         // 요청에 대한 응답을 받을 response
         BatchResponse response;
         try {
 
             // 알림 발송
-            response = FirebaseMessaging.getInstance().sendAll(messages);
-
+            response = FirebaseMessaging.getInstance().sendMulticast(message);
+//            failMessage(tokenList, response);
             // 요청에 대한 응답 처리
             if (response.getFailureCount() > 0) {
                 List<SendResponse> responses = response.getResponses();
@@ -96,4 +107,18 @@ public class FcmService {
             log.error("cannot send to memberList push message. error info : {}", e.getMessage());
         }
     }
+//    // 토큰이 유효하지 않아서 fcm 발송이 실패한 데이터 추출
+//    private void failMessage(List<String> mergeTokenList, BatchResponse response) {
+//        if (response.getFailureCount() > 0) {
+//            List<SendResponse> responses = response.getResponses();
+//            List<String> failedTokens = new ArrayList<>();
+//            for (int i = 0; i < responses.size(); i++) {
+//                if (!responses.get(i).isSuccessful()) {
+//                    failedTokens.add(mergeTokenList.get(i));
+//                }
+//            }
+//            log.info("======================= failedTokens : " + failedTokens + "=======================(추후 실패한 토큰은 삭제 시켜줘야함) -> 쓸데없는 알람이 가서 성능 저하를 일으킴");
+//        }
+//        log.info("======================= Success : " + response + "=======================");
+//    }
 }
